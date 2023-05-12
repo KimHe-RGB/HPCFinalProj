20744235c1027c7cd47b236a8f4b2a523a8ee7fe
Kim He <xh1131@nyu.edu>
2023-05-12 19:02:09 -0400

    Update testing_ground.m

--- Matlab/testing_ground.m
+++ Matlab/testing_ground.m
@@ -117,56 +117,6 @@ set(gca, 'YScale', 'log')
 set(gca, 'XDir','reverse')
 title('Spatial Error convergence, EE')
 
-%% part 2: multiple time step sizes & temporal convergence of Explicit methods
-CFL = 1/6;
-range = -8:1:-1;
-hxs = 2.^(range);
-hts = hxs.^2 * CFL;
-
-x0 = 0; xfinal = 1; 
-t0 = 0; tfinal = 0.25; 
-err_1s = zeros(length(hxs),1);
-err_2s = zeros(length(hxs),1);
-err_infs = zeros(length(hxs),1);
-
-for i = 1:length(hts)
-    hx = hxs(i); ht = hts(i);
-    % The grid I am using is x_{.5}, x_{1.5},..., x_{N-.5}
-    xs = x0+hx/2:hx:xfinal-hx/2; Nxs = length(xs);
-
-    % Solve with explicit solver
-    u = Heat1D_EE_solver([x0 hx xfinal],[t0 ht tfinal], ...
-        init_cond_f, diffusivity_f, force_f,...
-        lbry_f, rbry_f, ...
-        leftType, rightType);
-    real_u = u_sol_f(xs,tfinal);
-    
-    M = 1024;
-    u_int = reshape(interpft(u,M), [M,1]); 
-    real_u_int = reshape(interpft(real_u,M), [M,1]);
-
-    err_1s(i) = norm(u_int - real_u_int, 1);
-    err_2s(i) = norm(u_int - real_u_int, 2);
-    err_infs(i) = norm(u_int - real_u_int, "inf");
-end
-
-% error convergence order
-figure(2); clf; 
-hold on;
-plot(hts, err_1s, 'r-o', DisplayName="$$L_1$$ error")
-plot(hts, err_2s, 'g-o', DisplayName="$$L_2$$ error")
-plot(hts, err_infs, 'b-o', DisplayName="$$L_\infty$$ error")
-plot(hts, 40*hts.^2, 'k--o', DisplayName="$$O(\Delta t^2)$$");
-plot(hts, 10*hts, '--o', DisplayName="$$O(\Delta t)$$",Color=[.5 .5 .5]);
-
-figure(2); legend(interpreter="latex", Location="southwest");
-grid on
-xlabel("Time step size $$\Delta t$$",Interpreter="latex");
-ylabel("Error", Interpreter="latex");
-set(gca, 'XScale', 'log')
-set(gca, 'YScale', 'log')
-set(gca, 'XDir','reverse')
-title('Temporal Error convergence, EE')
 
 %% part 2: multiple time step sizes & temporal convergence of implicit methods
 range = -10:1:-3;
@@ -175,9 +125,8 @@ hxs = 2.^range;
 
 x0 = 0; xfinal = 1; 
 t0 = 0; tfinal = 1; 
-err_1s = zeros(length(hxs),1);
-err_2s = zeros(length(hxs),1);
-err_infs = zeros(length(hxs),1);
+err_ie_2s = zeros(length(hxs),1);
+err_cn_2s = zeros(length(hxs),1);
 
 for i = 1:length(hts)
     hx = hxs(i); ht = hts(i);
@@ -185,27 +134,30 @@ for i = 1:length(hts)
     xs = x0+hx/2:hx:xfinal-hx/2; Nxs = length(xs);
 
     % Solve with explicit solver
-    u = Heat1D_CN_solver([x0 hx xfinal],[t0 ht tfinal], ...
+    u_cn = Heat1D_CN_solver([x0 hx xfinal],[t0 ht tfinal], ...
+        init_cond_f, diffusivity_f, force_f,...
+        lbry_f, rbry_f, ...
+        leftType, rightType);
+    u_ie = Heat1D_IE_solver([x0 hx xfinal],[t0 ht tfinal], ...
         init_cond_f, diffusivity_f, force_f,...
         lbry_f, rbry_f, ...
         leftType, rightType);
     real_u = u_sol_f(xs,tfinal);
     
     M = 1024;
-    u_int = reshape(interpft(u,M), [M,1]); 
+    u_ie_int = reshape(interpft(u_ie,M), [M,1]); 
+    u_cn_int = reshape(interpft(u_cn,M), [M,1]); 
     real_u_int = reshape(interpft(real_u,M), [M,1]);
 
-    err_1s(i) = norm(u_int - real_u_int, 1);
-    err_2s(i) = norm(u_int - real_u_int, 2);
-    err_infs(i) = norm(u_int - real_u_int, "inf");
+    err_ie_2s(i) = norm(u_ie_int - real_u_int, 2);
+    err_cn_2s(i) = norm(u_cn_int - real_u_int, 2);
 end
 
 % error convergence order
 figure(2); clf; 
 hold on;
-plot(hts, err_1s, 'r-o', DisplayName="$$L_1$$ error")
-plot(hts, err_2s, 'g-o', DisplayName="$$L_2$$ error")
-plot(hts, err_infs, 'b-o', DisplayName="$$L_\infty$$ error")
+plot(hts, err_ie_2s, 'r-o', DisplayName="$$L_2$$ IE error")
+plot(hts, err_cn_2s, 'g-o', DisplayName="$$L_2$$ CN error")
 plot(hts, hts.^2, 'k--o', DisplayName="$$O(\Delta t^2)$");
 plot(hts, 10*hts, '--o', DisplayName="$$O(\Delta t)$$",Color=[.5 .5 .5]);
 
