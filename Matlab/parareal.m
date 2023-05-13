a25f4a51aa5230856aa2161617f232998f6e7277
Kim He <xh1131@nyu.edu>
2023-05-13 02:58:34 -0400

    update

--- Matlab/parareal.m
+++ Matlab/parareal.m
@@ -4,14 +4,18 @@
 % hit machine epsilon
 clear all;
 % expecting to evaluate in a very long time
-Total_T = 2^10; % 65536
+Total_T = 2^8; % 65536
 
 u_sol_f = @(x,t) exp(-pi^2*t/Total_T) * cos(pi*x/2);
 lbry_f = @(t) exp(-pi^2*t/Total_T);               % left boundary condition
-% rbry_f = @(t) -pi/2*exp(-pi^2*t/Total_T);         % right bdry cond for xfinal = 1,3,5,7 
-rbry_f = @(t) 0;                                    % right bdry cond for xfinal = 2,4,6,8..
 leftType = "Dirichlet";
-rightType = "Neumann";
+
+% rbry_f = @(t) -pi/2*exp(-pi^2*t/Total_T);         % right bdry cond for xfinal = 1,3,5,7 
+% rbry_f = @(t) 0;                                    % right bdry cond for xfinal = 2,4,6,8..
+% rightType = "Neumann";
+
+rbry_f = @(t) -exp(-pi^2*t/Total_T);
+rightType = "Dirichlet";
 diffusivity_f = @(x) 2 + cos(pi.*x);      % diffusivity function
 % forcing term
 force_f = @(x,t) ... 
@@ -55,15 +59,28 @@ init_cond_f = @(x) cos(pi*x/2);         % init condition function=u_sol(x,0)
 x0 = 0; xfinal = 2; 
 t0 = 0; tfinal = Total_T; 
 hx = 2^-5;
-ht = 2^-5;
+ht = 1;
 
 xs = x0+hx/2:hx:xfinal-hx/2; 
 init_u = init_cond_f(xs);
-fine_solver_u = Heat1D_IE_solver_coarse([x0 hx xfinal],[t0 ht tfinal], ...
+coarse_solver_u = Heat1D_IE_solver_coarse([x0 hx xfinal],[t0 ht tfinal], ...
+    init_u, diffusivity_f, force_f,...
+    lbry_f, rbry_f, ...
+    leftType, rightType, 1);
+fine_solver_u = Heat1D_CN_solver_fine([x0 hx xfinal],[t0 2^-5 tfinal], ...
     init_u, diffusivity_f, force_f,...
     lbry_f, rbry_f, ...
     leftType, rightType);
+real_u = u_sol_f(xs, tfinal);
 
+figure(2); 
+hold on;
+plot(xs, coarse_solver_u, 'b:', DisplayName="Coarse Solver");
+plot(xs, fine_solver_u, 'r-', DisplayName="Fine Solver");
+plot(xs, real_u, 'g--', DisplayName="Real");
+figure(2); legend(interpreter="latex", Location="southwest");
+xlabel("x",Interpreter="latex");
+ylabel("u(x,t)",Interpreter="latex");
 %%
 % Total T = 2^16
 % cores = 2^4       ==>     T interval / core ~ 2^12
@@ -103,8 +120,8 @@ x0 = 0; xfinal = 2;
 % error of CN ~ O(ht^2) + O(hx^2), to maximize we let ht = hx
 fine_ht = 2^-5;
 fine_hx = 2^-5;
-coarse_ht = 1;
-coarse_hx = fine_hx; % make this consistent with the fine grid
+coarse_ht = 1;          % coarse ht is much larger
+coarse_hx = fine_hx;    % make this consistent with the fine grid
 
 % the grid that we want to evaluate the PDE, 
 % here we use a staggered grid
@@ -115,71 +132,85 @@ Nxs = length(fine_xs);
 % initialization U^0_{n+1} = G(t_n, t_n, U^0_n) from n = 0,...core_num-1
 U = zeros(core_num,Nxs);
 U(1,:) = init_cond_f(fine_xs); 
-for i = 1:core_num-1
-    t0 = T0+(i-1)*interval_size; tfinal = T0+i*interval_size;
-    G_coarse = Heat1D_IE_solver_coarse( ...
-        [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], U(i,:), ...
-        diffusivity_f, force_f,...
-        lbry_f, rbry_f, ...
-        leftType, rightType);
-    U(i+1,:) = G_coarse;
-end
-% from the result of coarse grid, 
-% initialization of F^0_n+1 = F(t_n. t_n+1, G^0_n) from n = 0,...core_num-1
-% This part goes parallel
 F = zeros(core_num, Nxs);
-F(i,:) = init_cond_f(fine_xs);
-parfor i = 1:core_num-1
-    t0 = T0+(i-1)*interval_size; tfinal = T0+i*interval_size;
+F(1,:) = init_cond_f(fine_xs);
+% for i = 1:core_num-1
+%     t0 = T0+(i-1)*interval_size; tfinal = T0+i*interval_size;
+%     % coarse approximation from i -> i+1
+%     G_coarse = Heat1D_IE_solver_coarse( ...
+%         [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], U(i,:), ...
+%         diffusivity_f, force_f,...
+%         lbry_f, rbry_f, ...
+%         leftType, rightType);
+%     % initial value for i+1 is the ending value of i
+%     U(i+1,:) = G_coarse;
+% end
+% t0 = T0; tfinal = Total_T;
+% coarse_solver_u = Heat1D_IE_solver_coarse([x0 hx xfinal],[t0 ht tfinal], ...
+%     init_u, diffusivity_f, force_f,...
+%     lbry_f, rbry_f, ...
+%     leftType, rightType);
+% U(end,:) = coarse_solver_u;
 
-    F_fine_init = Heat1D_CN_solver_fine( ...
-        [x0 fine_hx xfinal],[t0 fine_ht tfinal], U(i,:), ...
-        diffusivity_f, force_f,...
-        lbry_f, rbry_f, ...
-        leftType, rightType);
-    F(i+1,:) = F_fine_init;
+t0 = T0; tfinal = Total_T;
+% coarse approximation from i -> i+1
+G_coarses = Heat1D_IE_solver_coarse( ...
+    [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], U(1,:), ...
+    diffusivity_f, force_f,...
+    lbry_f, rbry_f, ...
+    leftType, rightType, core_num);
+for i = 1:core_num-1
+    % initial value for i+1 is the ending value of i
+    U(i+1,:) = G_coarses(i+1,:);
 end
 
 % parareal iteration
-parareal_iters = 16;
+parareal_iters = 1;
 for iter = 1:parareal_iters
-    % coarse update
-    U(1,:) = init_cond_f(fine_xs); 
-    for i = 1:core_num-1
-        t0 = T0+(i-1)*interval_size; tfinal = T0+i*interval_size;
-        G_coarse = Heat1D_IE_solver_coarse( ...
-            [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], U(i,:), ...
-            diffusivity_f, force_f,...
-            lbry_f, rbry_f, ...
-            leftType, rightType);
-        G_coarse = reshape(G_coarse, [1, Nxs]);
-        U(i+1,:) = G_coarse + F(i,:) - U(i+1,:);
-    end
-    % go parallel
-    parfor i = 1:core_num-1
-        t0 = T0+(i-1)*interval_size; tfinal = T0+i*interval_size;
     
+    % from the result of coarse grid, compute the fine grid
+    % initialization of F^0_n+1 = F(t_n. t_n+1, G^0_n) from n = 0,...core_num-1
+    % This part goes parallel
+    parfor i = 1:core_num-1
+        t0_1 = T0+(i-1)*interval_size; tfinal_1 = T0+i*interval_size;
         F_fine = Heat1D_CN_solver_fine( ...
-            [x0 fine_hx xfinal],[t0 fine_ht tfinal], U(i,:), ...
+            [x0 fine_hx xfinal],[t0_1 fine_ht tfinal_1], U(i,:), ...
             diffusivity_f, force_f,...
             lbry_f, rbry_f, ...
             leftType, rightType);
         F(i+1,:) = F_fine;
     end
+
+    % coarse update
+    %     for j = 1:core_num-1
+    %         t0 = T0+(j-1)*interval_size; tfinal = T0+j*interval_size;
+    %         % new approximation from i -> i+1
+    %         G_coarse_new = Heat1D_IE_solver_coarse( ...
+    %             [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], F(j,:), ...
+    %             diffusivity_f, force_f,...
+    %             lbry_f, rbry_f, ...
+    %             leftType, rightType);
+    %         % Compute the correction
+    %         % Add the correction to the approximate solution on the coarse grid
+    %         G_coarse_new = reshape(G_coarse_new, [1, Nxs]);
+    %         U(j+1,:) = G_coarse_new - G(j+1,:) + F(j,:);
+    %     end
+    G_coarses_new = Heat1D_IE_solver_coarse( ...
+            [x0 coarse_hx xfinal],[t0 coarse_ht tfinal], U(1,:), ...
+            diffusivity_f, force_f,...
+            lbry_f, rbry_f, ...
+            leftType, rightType, core_num);
+    for i = 1:core_num-1
+        % initial value for i+1 is the ending value of i
+        U(i+1,:) = G_coarses_new(i+1,:) - U(i,:) + F(i,:);
+    end
 end
 
-parareal_u = F(end,:);
+parareal_u = U(end,:);
 real_u = u_sol_f(fine_xs, Total_T);
 
-%% Plot and visualize the solution
-figure(2); clf
-hold on;
-plot(fine_xs, parareal_u, 'g', DisplayName="Parareal");
-plot(xs, fine_solver_u, 'b--', DisplayName="Fine Solver");
-plot(fine_xs, real_u, 'r:', DisplayName="Real");
-figure(2); legend(interpreter="latex", Location="southwest");
-xlabel("x",Interpreter="latex");
-ylabel("u(x,t)",Interpreter="latex");
+% Plot and visualize the solution
+figure(2); plot(fine_xs, parareal_u, DisplayName="Parareal");
 
 %% Compute the parareal error with different number of iterations
 error = parareal_u-real_u;
@@ -194,7 +225,55 @@ title("Error in L2 norm");
 
 %% adapted from the Implicit Euler Solver
 % served as coarse solver
-function u = Heat1D_IE_solver_coarse(xspan, tspan, ...
+function u_outs = Heat1D_IE_solver_coarse(xspan, tspan, ...
+    icu, df, F,...
+    lbry, rbry, leftBCType, rightBCType, cores)
+
+    t0 = tspan(1); ht = tspan(2); tfinal = tspan(3);
+    x0 = xspan(1); hx = xspan(2); xfinal = xspan(end);    
+    % the grid that we want to evaluate the PDE, 
+    % here we use a staggered grid
+    xs = x0+hx/2:hx:xfinal-hx/2; Nxs = length(xs);
+        
+    % init loop
+    t = t0; i = 0; 
+    done = t >= tfinal;
+    
+    % initial condition 
+    assert(length(icu) == Nxs);
+    u = reshape(icu, [Nxs, 1]);
+
+    % spatial discretization
+    [L, r] = Spatial_Operator_Heat1D(xspan,df, leftBCType, rightBCType);
+    
+    % Linear system to solve for Implicit Euler
+    I = eye(Nxs); 
+    R = @(t) [lbry(t); zeros(Nxs-2,1); rbry(t)] .* r;
+    RHS = @(t) reshape(-F(xs, t), [Nxs,1]) + R(t);
+    Fn = @(u, t, ht) (I - ht*L) \ (u - ht*RHS(t));
+    
+    recordEveryNSteps = (tfinal - t0) / ht / cores;
+    u_outs = zeros(cores,Nxs); % record the ending value
+    count = 1;
+    while ~done
+        i = i + 1;
+        if(t + ht >= tfinal)
+            ht = tfinal - t;
+            done = true;
+        end
+        % One Step Method (Backward/Implicit Euler)
+        u = Fn(u, t, ht);
+        if (mod(i, recordEveryNSteps) == 0)
+            u_outs(count,:) = u;
+            count = count+1;
+        end
+        t = t + ht;
+    end
+end
+
+%% adapted from the Implicit Euler Solver
+% served as coarse solver
+function u = Heat1D_IE_solver(xspan, tspan, ...
     icu, df, F,...
     lbry, rbry, leftBCType, rightBCType)
 
@@ -205,7 +284,7 @@ function u = Heat1D_IE_solver_coarse(xspan, tspan, ...
     xs = x0+hx/2:hx:xfinal-hx/2; Nxs = length(xs);
         
     % init loop
-    t = t0; i = 1; 
+    t = t0; i = 0; 
     done = t >= tfinal;
     
     % initial condition 
